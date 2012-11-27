49fa65e7626115a454fc30a4dc8ea9646b177c38
Kevin Chan <kevin@joyent.com>
2012-11-27 18:25:24 +0800

    ADMINUI-1014 filter vms by uuid,status,server,alias and ram

--- public/js/views/vms.js
+++ public/js/views/vms.js
@@ -8,6 +8,18 @@ define(function(require) {
 
     var app = require('adminui');
 
+    var FilterForm = Backbone.View.extend({
+        events: {
+            'submit form': 'onSubmit'
+        },
+        onSubmit: function(e) {
+            e.preventDefault();
+
+            var params = this.$('form').serializeObject();
+            this.trigger('query', params);
+        }
+    });
+
     return Backbone.Marionette.ItemView.extend({
         name: 'vms',
         template: VmsTemplate,
@@ -23,6 +35,7 @@ define(function(require) {
         initialize: function(options) {
             this.collection = new Vms();
             this.listView = new VmsList({ collection: this.collection });
+            this.filterView = new FilterForm();
             this.collection.fetch();
         },
 
@@ -30,9 +43,15 @@ define(function(require) {
             app.vent.trigger('showview', 'provision-vm', {});
         },
 
+        query: function(params) {
+            this.collection.fetch({data: params});
+        },
+
         onRender: function() {
             this.listView.setElement(this.$('tbody')).render();
+            this.filterView.setElement(this.$('.vms-filter'));
 
+            this.bindTo(this.filterView, 'query', this.query, this);
             return this;
         }
     });
