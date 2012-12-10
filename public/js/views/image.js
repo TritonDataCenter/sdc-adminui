c2503ecbac87305b4c48d40beb68922438301eb2
Kevin Chan <kevin@joyent.com>
2012-12-10 19:49:06 +0800

    ADMINUI-1038 image activation

--- public/js/views/image.js
+++ public/js/views/image.js
@@ -7,9 +7,20 @@ define(function(require) {
         url: function() {
             return _.str.sprintf('images/%s', this.model.get('uuid'));
         },
-        modelEvents: {
-            'change': 'render'
+        events: {
+            'activate': 'onClickActivate'
         },
+        modelEvents: {
+            'change': 'render',
+            'error': 'onError'
+        },
+
+        templateHelpers: {
+            'activatable': function() {
+                return this.active === false;
+            }
+        },
+
         initialize: function(options) {
             if (options.uuid) {
                 this.model = new Img({uuid: options.uuid});
@@ -17,6 +28,18 @@ define(function(require) {
                 this.model = options.image;
             }
             this.model.fetch();
+        },
+        onError: function(model, res) {
+            app.vent.trigger('error', {
+                xhr: res,
+                context: 'images / imgapi'
+            });
+        },
+        onClickActivate: function() {
+            var self = this;
+            this.model.activate({success: function() {
+                self.model.fetch();
+            }});
         }
     });
 
