a3ece6c06d89c0ba89494f0c67d4c03e702459e7
Kevin Chan <kevin@joyent.com>
2013-01-11 17:23:05 -0800

    ADMINUI-1062 Add support for rack_identifier to server objects - update rack id

--- public/js/views/server.js
+++ public/js/views/server.js
@@ -1,5 +1,26 @@
 define(function(require) {
 
+    var ChangeRackFormTemplate = Handlebars.compile('<input class="input" type="text"><button class="btn btn-primary save">Save</button><button class="btn cancel">Cancel</button>');
+    var ChangeRackForm = Backbone.Marionette.ItemView.extend({
+        template: ChangeRackFormTemplate,
+        events: {
+            'click button.save': 'save',
+            'click button.cancel': 'cancel'
+        },
+        save: function() {
+            var self = this;
+            var rid = this.$('input').val();
+            this.model.set({rack_identifier: rid });
+            this.model.save(null, {success: function() {
+                self.trigger('save', rid);
+            }});
+        },
+        cancel: function() {
+            this.trigger('cancel');
+            this.remove();
+        }
+    });
+
     var Server = require('models/server');
     var BaseView = require('views/base');
     var ServerTemplate = require('text!tpl/server.html');
@@ -8,6 +29,10 @@ define(function(require) {
         sidebar: 'servers',
 
         template: ServerTemplate,
+
+        events: {
+            'click .change-rack-id': 'showChangeRackField'
+        },
 
         modelEvents: {
             'change': 'render'
@@ -46,6 +71,16 @@ define(function(require) {
                 return {name: k, size: v['Size in GB']};
             });
             return data;
+        },
+
+        showChangeRackField: function() {
+            var view = new ChangeRackForm({model: this.model});
+            this.bindTo(view, 'cancel', function() {
+                this.$('.rack td a').show();
+            }, this);
+            this.$('.rack td').append(view.el);
+            this.$('.rack td a').hide();
+            view.render();
         },
 
         url: function() {
