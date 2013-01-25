07ab5538abffd4e4a173369a41894b0869fd9205
Kevin Chan <kevin@joyent.com>
2013-01-25 15:00:44 -0800

    ADMINUI-1073 VM count on dashboard

--- public/js/views/dashboard.js
+++ public/js/views/dashboard.js
@@ -11,17 +11,21 @@ define(function(require) {
 	var AlarmsView = require('views/alarms');
 	var DashboardTemplate = require('text!tpl/dashboard.html');
 	var Dashboard = Backbone.Marionette.ItemView.extend({
+        id: 'page-dashboard',
 		name: 'dashboard',
 		url: 'dashboard',
 		template: DashboardTemplate,
 
 		initialize: function() {
-			console.log(adminui.user);
 			this.alarmsView = new AlarmsView({ userUuid: '00000000-0000-0000-0000-000000000000' });
 		},
 
 		onRender: function() {
 			this.alarmsView.setElement(this.$('#dashboard-alarms')).render();
+            var self = this;
+            $.getJSON("/_/stats/vm_count", function(res) {
+                self.$('.vm-count').html(res.total);
+            });
 
 			return this;
 		}
