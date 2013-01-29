3a56864cb54d242e0772d1aeb14e1d9ebc4f8b48
Kevin Chan <kevin@joyent.com>
2013-01-29 23:51:44 +0800

    ADMINUI-1045 Jobs browser, job details

--- public/js/models/job.js
+++ public/js/models/job.js
@@ -1,4 +1,5 @@
 define(function(require) {
+    var Backbone = require('backbone');
     var Job = Backbone.Model.extend({
         defaults: {
             "name": ""
--- public/js/views/jobs.js
+++ public/js/views/jobs.js
@@ -1,11 +1,32 @@
 define(function(require) {
+    var Backbone = require('backbone');
     var Jobs = require('models/jobs');
+
+    var JobDetailsViewTemplate = require('tpl!jobs-item-details');
+    var JobDetailsView = Backbone.Marionette.ItemView.extend({
+        template: JobDetailsViewTemplate,
+        id: 'job-item-details',
+        attributes: {
+            'class': 'modal'
+        },
+        onRender: function() {
+            this.$el.modal();
+        }
+    });
 
     var JobsItemViewTemplate = require('tpl!jobs-item');
     var JobsItemView = Backbone.Marionette.ItemView.extend({
         template: JobsItemViewTemplate,
-        tagName: 'tr'
+        tagName: 'tr',
+        events: {
+            "click button.details": "showJobDetails"
+        },
+        showJobDetails: function() {
+            var detailsView = new JobDetailsView({model: this.model});
+            detailsView.render();
+        }
     });
+
 
     var JobsTemplate = require('tpl!jobs');
     var JobsView = Backbone.Marionette.CompositeView.extend({
@@ -17,8 +38,6 @@ define(function(require) {
         initialize: function() {
             this.collection = new Jobs();
             this.collection.fetch();
-        },
-        onRender: function() {
         }
     });
 
