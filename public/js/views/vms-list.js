1afbbc264afff981b9f3f2c70746d248a9e8bdd6
Kevin Chan <kevin@joyent.com>
2013-02-08 20:30:09 +0800

    ADMINUI-1080 Fix problem displaying vms with no alias in vm list

--- public/js/views/vms-list.js
+++ public/js/views/vms-list.js
@@ -1,24 +1,28 @@
 define(function(require) {
 
-  var ItemTemplate = require('tpl!vms-list-item');
-  var adminui = require('adminui');
+    var ItemTemplate = require('tpl!vms-list-item');
+    var adminui = require('adminui');
 
-  var ItemView = Marionette.ItemView.extend({
-    tagName: 'tr',
-    template: ItemTemplate,
+    var ItemView = Marionette.ItemView.extend({
+        tagName: 'tr',
+        template: ItemTemplate,
 
-    events: {'click .alias a': 'navigateToVmDetails'},
+        events: {
+            'click .alias a': 'navigateToVmDetails'
+        },
 
-    initialize: function() {
-      _.bindAll(this);
-    },
+        initialize: function() {
+            _.bindAll(this);
+        },
 
-    navigateToVmDetails: function() {
-      adminui.vent.trigger('showview', 'vm', {vm:this.model});
-    }
-  });
+        navigateToVmDetails: function() {
+            adminui.vent.trigger('showview', 'vm', {
+                vm: this.model
+            });
+        }
+    });
 
-  return Backbone.Marionette.CollectionView.extend({
-    itemView: ItemView
-  });
+    return Backbone.Marionette.CollectionView.extend({
+        itemView: ItemView
+    });
 });
