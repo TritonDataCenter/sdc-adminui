118b857b619a3f960128337c2189938b8ef4c036
Kevin Chan <kevin@joyent.com>
2013-03-04 20:21:02 +0800

    ADMINUI-1103 when error occurs querying wfapi via vmapi (ie cannot connect to moray), it should be displayed error with details on jobs page

--- public/js/views/jobs.js
+++ public/js/views/jobs.js
@@ -1,9 +1,9 @@
 define(function(require) {
+    var adminui = require('adminui');
     var Backbone = require('backbone');
     var Jobs = require('models/jobs');
 
     var JobProgressView = require('views/job-progress');
-    
     var JobsItemViewTemplate = require('tpl!jobs-item');
     var JobsItemView = Backbone.Marionette.ItemView.extend({
         template: JobsItemViewTemplate,
@@ -25,9 +25,18 @@ define(function(require) {
         url: function() { return '/jobs'; },
         itemView: JobsItemView,
         itemViewContainer: 'tbody',
+
         initialize: function() {
             this.collection = new Jobs();
             this.collection.fetch();
+            this.bindTo(this.collection, 'error', this.onError);
+        },
+
+        onError: function(model, xhr) {
+            adminui.vent.trigger('error', {
+                context: 'workflow via vmapi',
+                xhr: xhr
+            });
         }
     });
 
