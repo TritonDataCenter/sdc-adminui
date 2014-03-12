cefd9565c172e3a15fa3bf14b1700370ce939d51
Kevin Chan <kevin@joyent.com>
2014-03-12 23:25:52 +0800

    ADMINUI-1597 empty state message

--- public/js/components/server-link-aggr/main.js
+++ public/js/components/server-link-aggr/main.js
@@ -13,24 +13,29 @@ var LinkAggregationsList = React.createClass({
     render: function() {
         return (<div className="link-aggr-list">
         {
-            this.props.linkAggregations.map(function(link) {
-                return <div key={link.id} className="link-aggr">
-                    <div className="link-aggr-name">{link.name}</div>
-                    <div className="link-aggr-interfaces">
-                    {
-                        link.macs.map(function(mac) {
-                            return <div key={mac} className="link-aggr-interface">
-                                {mac}
-                            </div>
-                        }, this)
-                    }
+            this.props.linkAggregations.length === 0 ? (
+                <div className="empty">There are no Aggregated Links on this node</div>
+            )
+            : (
+                this.props.linkAggregations.map(function(link) {
+                    return <div key={link.id} className="link-aggr">
+                        <div className="link-aggr-name">{link.name}</div>
+                        <div className="link-aggr-interfaces">
+                        {
+                            link.macs.map(function(mac) {
+                                return <div key={mac} className="link-aggr-interface">
+                                    {mac}
+                                </div>
+                            }, this)
+                        }
+                        </div>
+                        <div className="actions">
+                            <button onClick={this.props.onEdit.bind(null, link)} className="btn btn-edit"><i className="icon-pencil"></i> Edit</button>
+                            <button onClick={this.props.onDelete.bind(null, link)} className="btn btn-delete"><i className="icon-trash"></i> Delete</button>
+                        </div>
                     </div>
-                    <div className="actions">
-                        <button onClick={this.props.onEdit.bind(null, link)} className="btn btn-edit"><i className="icon-pencil"></i> Edit</button>
-                        <button onClick={this.props.onDelete.bind(null, link)} className="btn btn-delete"><i className="icon-trash"></i> Delete</button>
-                    </div>
-                </div>
-            }, this)
+                }, this)
+            )
         }
         </div>);
     }
