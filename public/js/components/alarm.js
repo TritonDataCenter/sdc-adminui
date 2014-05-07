b8193b7c5e4fa55289f92d02dc64ffa15e16593c
Kevin Chan <kevin@joyent.com>
2014-05-07 18:06:45 +0800

    ADMINUI-1687 pull up probe information

--- public/js/components/alarm.js
+++ public/js/components/alarm.js
@@ -190,38 +190,85 @@ module.exports = React.createClass({
             "image_uuid": "06b33b72-ce99-11e3-8fac-6bc848ca3215"
         };
 
-        var dfmt = "D MMM, HH:mm:ss";
-        var timeClosed;
-        if (alarm.closed) {
-            timeClosed = moment(alarm.timeClosed).utc().format(dfmt);
-        } else {
-            timeClosed = 'not closed';
-        }
+        return {};
+    },
+    componentWillMount: function() {
+        this.fetchAlarm();
+    },
+    getInitialState: function() {
         return {
-            timeOpened: moment(alarm.timeOpened).utc().format(dfmt),
-            timeClosed: timeClosed,
-            timeLastEvent: moment(alarm.timeLastEvent).utc().format(dfmt),
-            alarm: alarm,
-            vm: vm,
-            evt: evt,
-            server: cn
+            alarm: {},
+            probe: {},
+            server: null,
+            vm: null
         };
     },
+    fetchServer: function() {
+        api.get('/_/servers/'+this.state.alarm.machine).end(function(err, res) {
+            if (res.ok) {
+                this.setState({server: res.body});
+            }
+        }.bind(this));
+    },
+    fetchVm: function(id) {
+        api.get('/_/vms/'+this.state.alarm.machine).end(function(err, res) {
+            if (res.ok) {
+                this.setState({vm: res.body});
+            }
+        }.bind(this));
+    },
+    fetchProbe: function() {
+        var url = '/_/amon/probes/'+this.props.user + '/' + this.state.alarm.probe;
+        console.log('fetchProbe', this.state.alarm);
+        api.get(url).end(function(err, res) {
+            if (res.ok) {
+                this.setState({probe: res.body});
+            }
+        }.bind(this));
+    },
+    fetchAlarm: function() {
+        var url = '/_/amon/alarms/'+this.props.user+'/' + this.props.id;
+        api.get(url).end(function(err, res) {
+            var alarm = res.body;
+            this.setState({alarm: alarm});
+            if (alarm.probe) {
+                this.fetchProbe();
+            }
+            if (alarm.machine) {
+                this.fetchServer(id);
+                this.fetchVm(id);
+            }
+        }.bind(this));
+    },
+
     render: function() {
+        console.log(this.state);
+        var alarm = this.state.alarm;
+        var probe = this.state.probe;
+
+        var dfmt = "D MMM, HH:mm:ss";
+        var timeClosed =
+            (alarm.closed) ? moment(alarm.timeClosed).utc().format(dfmt)
+            : 'not closed';
+        var timeOpened = moment(alarm.timeOpened).utc().format(dfmt);
+        var timeLastEvent = moment(alarm.timeLastEvent).utc().format(dfmt);
+
         return (
             <div id="page-alarm">
                 <div className="page-header row">
                 <div className="state col-sm-12">
                     {
-                        this.props.closed ?
+                        alarm.closed ?
                         <span className="brand-info" className="closed">CLOSED</span>
                         :
                         <span className="brand-info" className="opened">OPENED</span>
                     }
                 </div>
                 <h1 className="col-sm-12">
-                        <span className="probe-name"><i className="fa fa-bell"></i> Alarm {this.props.alarm.monitor}</span>
-                        <small className="uuid"> {this.props.alarm.user}.{this.props.alarm.id}</small>
+                    <i className="fa fa-bell"></i> &nbsp;
+                    <span className="probe-name">{this.state.probe.name}</span>
+                    &nbsp; <span className="probe-type">{this.state.probe.type}</span>
+                    <small className="uuid"> {this.props.user}-{this.state.alarm.id}</small>
                     </h1>
                 </div>
 
@@ -229,48 +276,94 @@ module.exports = React.createClass({
                     <div className="col-sm-9">
                         <div className="row">
                             <div className="col-md-4">
-                                <div className="lbl"><i className="fa fa-clock-o"></i> Opened at</div>
-                                <div className="val">{this.props.timeOpened}</div>
-                            </div>
-                            <div className="col-md-4">
-                                <div className="lbl">
-                                    {
-                                        this.props.closed ?
-                                        <div><i className="fa fa-clock-o"></i> Closed at</div> :
-                                        <div><i className="fa fa-clock-o"></i> Not Closed</div>
-                                    }
+                                <div className="widget-content">
+                                    <div className="lbl"><i className="fa fa-clock-o"></i> Opened at</div>
+                                    <div className="val">{timeOpened}</div>
                                 </div>
-                                    {
-                                        this.props.closed ? <div className="val">{this.props.timeClosed}</div>
-                                        : <button className="btn btn-link">Close Alarm <i className="fa fa-power-off"></i></button>
-                                    }
                             </div>
                             <div className="col-md-4">
-                                <div className="lbl"><i className="fa fa-clock-o"></i> Last Event</div>
-                                <div className="val">{this.props.timeLastEvent}</div>
+                                <div className="widget-content">
+                                    <div className="lbl">
+                                        {
+                                            alarm.closed ?
+                                            <div><i className="fa fa-clock-o"></i> Closed at</div> :
+                                            <div><i className="fa fa-clock-o"></i> Not Closed</div>
+                                        }
+                                    </div>
+                                        {
+                                            alarm.closed ? <div className="val">{timeClosed}</div>
+                                            : <button className="btn btn-link">Close Alarm <i className="fa fa-power-off"></i></button>
+                                        }
+                                </div>
+                            </div>
+                            <div className="col-md-4">
+                                <div className="widget-content">
+                                    <div className="lbl"><i className="fa fa-clock-o"></i> Last Event</div>
+                                    <div className="val">{timeLastEvent}</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-sm-12 faults-container">
+                            {
+                                this.state.alarm.faults ? this.state.alarm.faults.map(this.renderFault) : ''
+                            }
                             </div>
                         </div>
                     </div>
 
                     <div className="col-sm-3">
-                        <div className="machine">
-                            <h6>Machine <span className="alias">{this.props.vm.alias}</span></h6>
-                            <div className="state pull-right">
-                                <span className={this.props.vm.state}>{this.props.vm.state}</span>
-                            </div>
-                            <div className="uuid">{this.props.vm.uuid}</div>
-                        </div>
+                        {
+                            this.state.vm ?
+                            <div className="machine">
+                                <h6>Machine <span className="alias">{vm.alias}</span></h6>
+                                <div className="state pull-right">
+                                    <span className={vm.state}>{vm.state}</span>
+                                </div>
+                                <div className="uuid">{vm.uuid}</div>
+                            </div> : ''
+                        }
 
-                        <div className="server">
-                            <h6>Machine <span className="hostname">{this.props.server.hostname}</span></h6>
-                            <div className="state pull-right">
-                                <span className={this.props.server.status}>{this.props.server.status}</span>
-                            </div>
-                            <div className="uuid">{this.props.server.uuid}</div>
-                        </div>
+                        {
+                            this.state.server ?
+                            <div className="machine">
+                                <h6>Machine <span className="alias">{server.hostname}</span></h6>
+                                <div className="state pull-right">
+                                    <span className={vm.state}>{server.status}</span>
+                                </div>
+                                <div className="uuid">{server.uuid}</div>
+                            </div> : ''
+                        }
+
+                        { this.renderProbe() }
                     </div>
                 </div>
             </div>
         );
+    },
+    renderFault: function(f) {
+        return <div className="fault">
+        <div className="event">
+        {JSON.stringify(f.event.data, null, 2)}
+        </div>
+        </div>
+    },
+    renderProbe: function() {
+        var probe = this.state.probe;
+        var contacts = [];
+        if (probe.contacts) {
+            contacts = probe.contacts.map(function(c) {
+                return <span className={c}>{c}</span>
+            });
+        }
+        return <div className="probe">
+            <h6>Probe <span className="alias">{probe.name}</span></h6>
+            <div className="type">type: {probe.type}</div>
+            <div className="uuid">{probe.uuid}</div>
+            <div className="state">
+                {contacts}
+            </div>
+        </div>
     }
 });
