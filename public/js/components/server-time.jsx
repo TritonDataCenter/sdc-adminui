396ec916a4df91b24d7631a9b812a9db8cddec98
Kevin Chan <kevin@joyent.com>
2014-05-27 22:49:55 +0800

    New ServerTime Component

---
+++ public/js/components/server-time.jsx
@@ -0,0 +1,22 @@
+var React = require('react');
+var moment = require('moment');
+
+var ServerTime = React.createClass({
+    componentWillMount: function() {
+        this._getTime();
+    },
+    componentDidMount: function() {
+        this._interval = setInterval(this._getTime.bind(this), 1000);
+    },
+    _getTime: function() {
+        this.setState({time: moment().utc().format("MMM D h:mm") });
+    },
+    render: function() {
+        return <div id="server-time"><i className="fa fa-clock-o"></i> UTC <time>{this.state.time}</time></div>
+    },
+    componentWillUnmount: function() {
+        clearInterval(this._interval);
+    }
+});
+
+module.exports = ServerTime;
