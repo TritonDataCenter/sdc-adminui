776a6d47e3a496d2f9e7aa4f9fb5b4aa89f489c8
Kevin Chan <kevin@joyent.com>
2014-05-28 16:11:32 +0800

    ADMINUI-943 Icons for Local nav

--- public/js/components/localnav.jsx
+++ public/js/components/localnav.jsx
@@ -24,10 +24,10 @@ var SecondaryNav = React.createClass({
             <ul className="nav">
                 <li className="nav-header">Compute</li>
                 <li className={this._classesFor('dashboard')} onClick={this._clickedMenuItem} data-view="dashboard"><a href="/dashboard"><i className="fa fa-home"></i> Dashboard</a></li>
-                <li className={this._classesFor('vms')} onClick={this._clickedMenuItem} data-view="vms"><a href="/vms">Virtual Machines</a></li>
-                <li className={this._classesFor('servers')} onClick={this._clickedMenuItem} data-view="servers"><a href="/servers">Servers</a></li>
-                <li className={this._classesFor('images')} onClick={this._clickedMenuItem} data-view="images"><a href="/images">Images</a></li>
-                <li className={this._classesFor('packages')} onClick={this._clickedMenuItem} data-view="packages"><a href="/packages">Packages</a></li>
+                <li className={this._classesFor('vms')} onClick={this._clickedMenuItem} data-view="vms"><a href="/vms"><i className="fa fa-cubes"></i> Virtual Machines</a></li>
+                <li className={this._classesFor('servers')} onClick={this._clickedMenuItem} data-view="servers"><a href="/servers"><i className="fa fa-list"></i> Servers</a></li>
+                <li className={this._classesFor('images')} onClick={this._clickedMenuItem} data-view="images"><a href="/images"><i className="fa fa-photo"></i> Images</a></li>
+                <li className={this._classesFor('packages')} onClick={this._clickedMenuItem} data-view="packages"><a href="/packages"><i className="fa fa-codepen"></i> Packages</a></li>
 
                 <li className="nav-header">Infrastructure</li>
                 <li className={this._classesFor('jobs')} onClick={this._clickedMenuItem} data-view="jobs"><a href="/jobs">Jobs</a></li>
