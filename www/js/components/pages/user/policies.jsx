eae9327f0e3fa8ce8e667cec979885fedcaf2038
Kevin Chan <kevin@joyent.com>
2014-06-18 17:46:57 +0800

    ADMINUI-1747 list, add, edit, and remove account policies

--- www/js/components/pages/user/policies.jsx
+++ www/js/components/pages/user/policies.jsx
@@ -2,111 +2,10 @@ var React = require('react');
 var PropTypes = React.PropTypes;
 
 var api = require('../../../request');
+var UserPolicyForm = require('./policy-form');
+var adminui = require('../../../adminui');
 
-var UserPolicyForm = React.createClass({
-    propTypes: {
-        handleSavePolicy: PropTypes.func.isRequired,
-        handleCancel: PropTypes.func,
-        error: PropTypes.object
-    },
-    getDefaultProps: function() {
-        return {
-            handleCancel: function() {},
-            error: null
-        };
-    },
-    getInitialState: function() {
-        return {
-            rules: ['']
-        };
-    },
-    render: function() {
-        return (
-            <div className="panel">
-                <div className="panel-body">
-                { this.props.error && <div className="alert alert-danger">{this.props.error.message}</div> }
-                <form className="form form-horizontal" onSubmit={this._handleSubmit}>
-                    <div className="form-group">
-                        <div className="control-label col-sm-4">Policy Name</div>
-                        <div className="controls col-sm-6">
-                            <input type="text" placeholder="name of account policy" onChange={this._handleChangeName} value={this.state.name} className="form-control" />
-                        </div>
-                    </div>
 
-                   <div className="form-group">
-                        <div className="control-label col-sm-4">Policy Description</div>
-                        <div className="controls col-sm-6">
-                            <input type="text" placeholder="describe this policy" onChange={this._handleChangeDescription} value={this.state.description} name="name" className="form-control" />
-                        </div>
-                    </div>
-
-                   <div className="form-group">
-                        <div className="control-label col-sm-4">Rules</div>
-                        <div className="controls col-sm-6">
-                            {
-                                this.state.rules.map(function(r, i) {
-                                    return <div key={i} className="input-group">
-                                        <input className="form-control" type="text" key={i} onChange={this._handleChangeRule} value={r} data-index={i} />
-                                        <div className="input-group-btn">
-                                        <button type="button" className="btn btn-link" data-index={i} onClick={this._handleRemoveRule}><i className="fa fa-times"></i></button>
-                                        </div>
-                                    </div>;
-                                }, this)
-                            }
-                            <button type="button" onClick={this._handleAddAnotherRule} className="btn btn-link"><i className="fa fa-plus" /> Add another rule</button>
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="input-group col-sm-offset-4 col-sm-6">
-                            <button type="submit" onClick={this._handleSubmit} className="btn btn-primary">Save Policy</button>
-                            <button type="button" onClick={this._handleCancel} className="btn btn-default">Cancel</button>
-                        </div>
-                    </div>
-                </form>
-                </div>
-            </div>
-        );
-    },
-    _handleCancel: function() {
-        this.props.handleCancel();
-    },
-    _handleRemoveRule: function(e) {
-        e.preventDefault();
-        var i = e.target.getAttribute('data-index');
-        if (i > -1)  {
-            var rules = this.state.rules;
-            rules = rules.splice(i, 1);
-            console.log(i, rules);
-            this.setState({rules: rules});
-        }
-    },
-    _handleSubmit: function(e) {
-        e.preventDefault();
-        var policy = this.state;
-        this.props.handleSavePolicy(policy);
-    },
-    _handleChangeRule: function(e) {
-        var r = e.target.value;
-        var i = e.target.getAttribute('data-index');
-        var rules = this.state.rules;
-        rules[i] = r;
-        this.setState({rules: rules});
-    },
-    _handleChangeName: function(e) {
-        var n = e.target.value;
-        this.setState({name: n});
-    },
-    _handleChangeDescription: function(e) {
-        var d = e.target.value;
-        this.setState({description: d});
-    },
-    _handleAddAnotherRule: function(e) {
-        e.preventDefault();
-        var rules = this.state.rules;
-        rules.push('');
-        this.setState({rules: rules});
-    }
-});
 
 var UserPolicies = React.createClass({
     propTypes: {
@@ -126,15 +25,26 @@ var UserPolicies = React.createClass({
     },
     _handleSavePolicy: function(policy) {
         var p = _.clone(policy);
+        var req;
         p.rule = p.rules;
         delete p.rules;
-        api.post(this._policiesApiUrl()).send(p).end(function(res) {
+        if (p.uuid) {
+            req = api.put(_.str.sprintf('%s/%s', this._policiesApiUrl(), p.uuid));
+        } else {
+            req = api.post(this._policiesApiUrl());
+        }
+        req.send(p).end(function(res) {
             if (res.ok) {
                 this.setState({
                     policyFormError: null,
+                    policyFormInitialPolicy: null,
                     policyForm: false
                 });
-                console.log('success create policy', res.body);
+                adminui.vent.trigger('notification', {
+                    level: 'success',
+                    message: _.str.sprintf('Policy %s saved successfully.', res.body.name)
+                });
+                console.log('success save policy', res.body);
                 this._fetchPolicies();
             } else {
                 console.log(res.error, res.body);
@@ -166,18 +76,22 @@ var UserPolicies = React.createClass({
         return <div key={p.uuid} className="panel policy">
             <div className="panel-body">
                 <div className="row">
-                    <div className="col-sm-2">
+                    <div className="col-xs-2">
                         <div className="name">{p.name}</div>
                     </div>
-                    <div className="col-sm-4">
+                    <div className="col-xs-4">
                         <div className="description">{p.description}</div>
                     </div>
-                    <div className="col-sm-6">
+                    <div className="col-xs-5">
                         {
                             p.rules.map(function(r) {
                                 return <div key={r} className="rule"><span className="r">{r}</span></div>;
                             })
                         }
+                    </div>
+                    <div className="col-xs-1">
+                        <button type="button" onClick={this._handleEditPolicy.bind(null, p)} className="btn btn-link edit-policy"><i className="fa fa-pencil"></i></button>
+                        <button type="button" onClick={this._handleRemovePolicy.bind(null, p)} className="btn btn-link remove-policy"><i className="fa fa-trash-o"></i></button>
                     </div>
                 </div>
             </div>
@@ -203,6 +117,7 @@ var UserPolicies = React.createClass({
             </h3>
             { this.state.policyForm && <UserPolicyForm
                 error={this.state.policyFormError}
+                initialPolicy={this.state.policyFormInitialPolicy}
                 handleCancel={this._handleCancelNewPolicy}
                 handleSavePolicy={this._handleSavePolicy} /> }
             <div className="policies">
@@ -217,6 +132,28 @@ var UserPolicies = React.createClass({
                 }
             </div>
         </div>);
+    },
+    _handleRemovePolicy: function(p) {
+        var msg = _.str.sprintf("Removing %s will remove this policy from roles it's associated with. Are you sure you want to proceed?");
+        var c = window.confirm(msg);
+        if (c) {
+            var url = _.str.sprintf('%s/%s', this._policiesApiUrl(), p.uuid);
+            api.del(url).end(function(res) {
+                if (res.ok) {
+                    adminui.vent.trigger('notification', {
+                        level: 'success',
+                        message: _.str.sprintf('Policy %s removed successfully.', p.name)
+                    });
+                    this._fetchPolicies();
+                }
+            });
+        }
+    },
+    _handleEditPolicy: function(p) {
+        this.setState({
+            policyForm: true,
+            policyFormInitialPolicy: p
+        });
     },
     _handleCancelNewPolicy: function() {
         this.setState({policyForm: false});
---
+++ www/js/components/pages/user/policy-form.jsx
@@ -0,0 +1,119 @@
+/** @jsx React.DOM */
+var React = require('react');
+var PropTypes = React.PropTypes;
+var UserPolicyForm = React.createClass({
+    propTypes: {
+        handleSavePolicy: PropTypes.func.isRequired,
+        handleCancel: PropTypes.func,
+        error: PropTypes.object,
+        initialPolicy: PropTypes.object,
+    },
+    getDefaultProps: function() {
+        return {
+            handleCancel: function() {},
+            error: null
+        };
+    },
+    getInitialState: function() {
+        var state;
+        if (this.props.initialPolicy) {
+            state = this.props.initialPolicy;
+        } else {
+            state = {
+                rules: ['']
+            };
+        }
+        return state;
+    },
+    render: function() {
+        return (
+            <div className="panel">
+                <div className="panel-body">
+                <div className="panel-title">
+                    { this.state.uuid ? 'Edit Policy' : 'New Policy'}
+                </div>
+                { this.props.error && <div className="alert alert-danger">{this.props.error.message}</div> }
+                <form className="form form-horizontal" onSubmit={this._handleSubmit}>
+                    <div className="form-group">
+                        <div className="control-label col-sm-4">Policy Name</div>
+                        <div className="controls col-sm-6">
+                            <input type="text" placeholder="name of account policy" onChange={this._handleChangeName} value={this.state.name} className="form-control" />
+                        </div>
+                    </div>
+
+                   <div className="form-group">
+                        <div className="control-label col-sm-4">Policy Description</div>
+                        <div className="controls col-sm-6">
+                            <input type="text" placeholder="describe this policy" onChange={this._handleChangeDescription} value={this.state.description} name="name" className="form-control" />
+                        </div>
+                    </div>
+
+                   <div className="form-group">
+                        <div className="control-label col-sm-4">Rules</div>
+                        <div className="controls col-sm-6">
+                            {
+                                this.state.rules.map(function(r, i) {
+                                    return <div key={i} className="input-group">
+                                        <input className="form-control" type="text" key={i} onChange={this._handleChangeRule} value={r} data-index={i} />
+                                        <div className="input-group-btn">
+                                            <button type="button" className="btn btn-link" data-index={i} onClick={this._handleRemoveRule}><i className="fa fa-times"></i></button>
+                                        </div>
+                                    </div>;
+                                }, this)
+                            }
+                            <button type="button" onClick={this._handleAddAnotherRule} className="btn btn-link"><i className="fa fa-plus" /> Add another rule</button>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <div className="input-group col-sm-offset-4 col-sm-6">
+                            <button type="submit" onClick={this._handleSubmit} className="btn btn-primary">Save Policy</button>
+                            <button type="button" onClick={this._handleCancel} className="btn btn-default">Cancel</button>
+                        </div>
+                    </div>
+                </form>
+                </div>
+            </div>
+        );
+    },
+    _handleCancel: function() {
+        this.props.handleCancel();
+    },
+    _handleRemoveRule: function(e) {
+        e.preventDefault();
+        var i = e.target.getAttribute('data-index');
+        if (i > -1)  {
+            var rules = this.state.rules;
+            rules = rules.splice(i, 1);
+            console.log(i, rules);
+            this.setState({rules: rules});
+        }
+    },
+    _handleSubmit: function(e) {
+        e.preventDefault();
+        var policy = this.state;
+        this.props.handleSavePolicy(policy);
+    },
+    _handleChangeRule: function(e) {
+        var r = e.target.value;
+        var i = e.target.getAttribute('data-index');
+        var rules = this.state.rules;
+        rules[i] = r;
+        this.setState({rules: rules});
+    },
+    _handleChangeName: function(e) {
+        var n = e.target.value;
+        this.setState({name: n});
+    },
+    _handleChangeDescription: function(e) {
+        var d = e.target.value;
+        this.setState({description: d});
+    },
+    _handleAddAnotherRule: function(e) {
+        e.preventDefault();
+        var rules = this.state.rules;
+        rules.push('');
+        this.setState({rules: rules});
+    }
+});
+
+module.exports = UserPolicyForm;
