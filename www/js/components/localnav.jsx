b8bd197e275197ebebfc8eaeb22727b64cb91d52
Kevin Chan <kevin@joyent.com>
2014-06-24 16:07:47 +0800

    ADMINUI-1767 image repository selection control partially cut off

--- www/js/components/localnav.jsx
+++ www/js/components/localnav.jsx
@@ -49,9 +49,9 @@ var SecondaryNav = React.createClass({
                 { this.mantaNav() }
 
                 <li className="nav-header">Infrastructure</li>
-                <li className={this._classesFor('jobs')} onClick={this._clickedMenuItem} data-view="jobs"><a href="/jobs">Jobs</a></li>
-                <li className={this._classesFor('services')} onClick={this._clickedMenuItem} data-view="services"><a href="/services">Services</a></li>
                 <li className={this._classesFor('networking')} onClick={this._clickedMenuItem} data-view="networking"><a href="/networks">Networking</a></li>
+                <li className={this._classesFor('jobs')} onClick={this._clickedMenuItem} data-view="jobs"><a href="/jobs">Workflow</a></li>
+                <li className={this._classesFor('services')} onClick={this._clickedMenuItem} data-view="services"><a href="/services">Services</a></li>
             </ul>
         </div>);
     }
