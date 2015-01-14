0e03988c42f9b42f437167e34101b25c14c646b5
Kevin Chan <kevin@joyent.com>
2015-01-15 00:40:51 +0800

    ADMINUI-1713 execute action on multiple vm instances

--- www/js/components/vms-list.jsx
+++ www/js/components/vms-list.jsx
@@ -12,8 +12,10 @@
 
 
 var adminui = require('../adminui');
-
+var _ = require('underscore');
+var api = require('../request');
 var React = require('react');
+var cx = React.addons.classSet;
 
 var ServerLink = require('./server-link');
 var ImageLnk = require('./image-link');
@@ -21,7 +23,7 @@ var UserLink = require('./user-link');
 
 var Vms = require('../models/vms');
 var JSONExport = require('./json-export');
-
+var BatchJobProgress = require('./batch-job-progress');
 
 
 var VmsList = React.createClass({
@@ -40,7 +42,10 @@ var VmsList = React.createClass({
         });
     },
     getInitialState: function() {
-        return {loading: true};
+        return {
+            loading: true,
+            selected: []
+        };
     },
     _onRequest: function() {
         this.setState({loading: true});
@@ -55,80 +60,44 @@ var VmsList = React.createClass({
             this.setState({'exported': exported});
         }.bind(this));
     },
+
     handleDismissExport: function() {
         this.setState({'exported': false});
     },
+
     handleLoadMore: function() {
         if (this.props.collection.hasNext()) {
             this.props.collection.next();
             this._requests.push(this.props.collection.fetch({remove: false}));
         }
     },
+
     handleLoadAll: function() {
         this.props.collection.pagingParams.perPage = null;
         this._requests.push(this.props.collection.fetch({remove: false}));
     },
-    render: function() {
-        if (this.state.loading) {
-            return <div className="vms-list">
-            <div className="zero-state">Retrieving Virtual Machines</div>
-            </div>;
-        }
-        if (! this.props.collection.length) {
-            return <div className="zero-state">No Virtual Machines were found matching specified criteria</div>;
-        }
-
-        return <div className="vms-list">
-                <div className="vms-list-header">
-                    <div className="title">
-                        Showing <span className="current-count">{this.props.collection.length}</span> of <span className="record-count">{this.props.collection.objectCount}</span> Virtual Machines<br/>
-                    </div>
-                    <div className="actions">
-                        {this.props.collection.objectCount ?
-                            <a onClick={this.handleExport} className="export">Export (<span className="record-count">{this.props.collection.length}</span>) <i className="fa fa-share-square"></i></a>
-                            : null }
-                    </div>
-                </div>
-                <table className="table">
-                    <tbody>{ this.props.collection.map(this.renderVm, this) }</tbody>
-                    <caption className="row" style={{visibility: this.state.loading ? 'hidden': 'visible'}}>
-                        <div className="col-sm-offset-6 col-sm-4" style={{paddingLeft: 0, paddingRight:0}}>
-                        {
-                            this.props.collection.objectCount && this.props.collection.objectCount !== this.props.collection.length ?
-                            <a onClick={this.handleLoadMore}
-                                className="more btn btn-block btn-success"><i className="fa fa-arrow-circle-down"></i> &nbsp; Load More</a>
-                            : null
-                        }
-                        </div>
-                        <div className="col-sm-2" style={{paddingLeft: 0}}>
-                        {
-                            this.props.collection.objectCount && this.props.collection.objectCount !== this.props.collection.length ?
-                            <a onClick={this.handleLoadAll}
-                                className="all btn btn-block btn-info"><i className="fa fa-arrow-circle-down"></i>
-                                    &nbsp; Load All (<span className="record-count">{this.props.collection.objectCount}</span>)</a>
-                            : null
-                        }
-                        </div>
-                    </caption>
-                </table>
-                {
-                    this.state.exported ? <div className="export-container">
-                        <JSONExport description="Virtual Machines grouped by owner" data={this.state.exported} onRequestHide={this.handleDismissExport} />
-                    </div> : null
-                }
-            </div>;
+    _handleCloseJobs: function() {
+        this.setState({jobs: null});
     },
-    navigateToVmPage: function(e) {
-        if (e.metaKey || e.ctrlKey) {
-            return;
-        }
-        e.preventDefault();
-
-        var vmModel = this.props.collection.get(e.target.getAttribute('data-vm-uuid'));
-        adminui.router.showVm(vmModel.get('uuid'));
+    _handleClearSelection: function() {
+        this.setState({selected: []});
     },
-    navigateToOwnerDetailsPage: function(user) {
-        adminui.vent.trigger('showcomponent', 'user', { user: user });
+
+    renderActionBar: function() {
+        var numSelectedMachines = this.state.selected.length;
+        return <div className="actionbar row">
+            <div className="actionbar-items col-sm-4">
+                {numSelectedMachines} Virtual Machine{numSelectedMachines > 1 ? 's' : ''} selected
+            </div>
+            <div className="actionbar-actions col-sm-6">
+                <a data-action="reboot" onClick={this._handleAction}><i className="fa fa-fw fa-refresh" /> Reboot</a>
+                <a data-action="stop" onClick={this._handleAction}><i className="fa fa-fw fa-power-off" /> Stop</a>
+                { /* <a data-action="apply-tags" onClick={this._handleAction}><i className="fa fa-fw fa-tag" /> Apply Tags</a> */}
+            </div>
+            <div className="actionbar-clear col-sm-2">
+                <a onClick={this._handleClearSelection}><i className="fa fa-times" /> Clear</a>
+            </div>
+        </div>;
     },
     renderVm: function(vmModel) {
         var vm = vmModel.toJSON();
@@ -139,11 +108,17 @@ var VmsList = React.createClass({
         var ips = vm.nics.map(function(n) {
             return n.ip;
         });
+        var selected = _.findWhere(this.state.selected, {uuid: vm.uuid});
 
-        return <tr key={vm.uuid}>
+        return <tr key={vm.uuid} className={ cx({selected: selected})}>
+            <td className="select">
+                <input onChange={this._handleSelectVm} data-vm-uuid={vm.uuid} type="checkbox" checked={selected} className="input" />
+            </td>
+
             <td className="status">
                 <span className={'state ' + vm.state}>{vm.state}</span>
             </td>
+
             <td className="alias">
                 <a href={vmUrl}
                     data-vm-uuid={vm.uuid}
@@ -182,6 +157,134 @@ var VmsList = React.createClass({
                 </div>
             </td>
         </tr>;
+    },
+    render: function() {
+        if (this.state.loading) {
+            return <div className="vms-list">
+                <div className="zero-state">Retrieving Virtual Machines</div>
+            </div>;
+        }
+
+        if (! this.props.collection.length) {
+            return <div className="zero-state">No Virtual Machines were found matching specified criteria</div>;
+        }
+
+        console.log('[VmsList] state', this.state);
+
+        return <div className="vms-list">
+                { this.state.selected.length > 0 ? this.renderActionBar() : null }
+                { this.state.jobs ? <BatchJobProgress {...this.state.jobs} onClose={this._handleCloseJobs} /> : null }
+
+                <div className="vms-list-header">
+                    <div className="title">
+                        Showing <span className="current-count">{this.props.collection.length}</span> of <span className="record-count">{this.props.collection.objectCount}</span> Virtual Machines<br/>
+                    </div>
+                    <div className="actions">
+                        {this.props.collection.objectCount ?
+                            <a onClick={this.handleExport} className="export">Export (<span className="record-count">{this.props.collection.length}</span>) <i className="fa fa-share-square"></i></a>
+                            : null }
+                    </div>
+                </div>
+
+                <table className="table">
+                    <tbody>{ this.props.collection.map(this.renderVm, this) }</tbody>
+                    <caption className="row" style={{visibility: this.state.loading ? 'hidden': 'visible'}}>
+                        <div className="col-sm-offset-6 col-sm-4" style={{paddingLeft: 0, paddingRight:0}}>
+                        {
+                            this.props.collection.objectCount && this.props.collection.objectCount !== this.props.collection.length ?
+                            <a onClick={this.handleLoadMore}
+                                className="more btn btn-block btn-success"><i className="fa fa-arrow-circle-down"></i> &nbsp; Load More</a>
+                            : null
+                        }
+                        </div>
+                        <div className="col-sm-2" style={{paddingLeft: 0}}>
+                        {
+                            this.props.collection.objectCount && this.props.collection.objectCount !== this.props.collection.length ?
+                            <a onClick={this.handleLoadAll}
+                                className="all btn btn-block btn-info"><i className="fa fa-arrow-circle-down"></i>
+                                    &nbsp; Load All (<span className="record-count">{this.props.collection.objectCount}</span>)</a>
+                            : null
+                        }
+                        </div>
+                    </caption>
+                </table>
+                {
+                    this.state.exported ? <div className="export-container">
+                        <JSONExport description="Virtual Machines grouped by owner" data={this.state.exported} onRequestHide={this.handleDismissExport} />
+                    </div> : null
+                }
+            </div>;
+    },
+
+    navigateToVmPage: function(e) {
+        if (e.metaKey || e.ctrlKey) {
+            return;
+        }
+        e.preventDefault();
+
+        var vmModel = this.props.collection.get(e.target.getAttribute('data-vm-uuid'));
+        adminui.router.showVm(vmModel.get('uuid'));
+    },
+
+    navigateToOwnerDetailsPage: function(user) {
+        adminui.vent.trigger('showcomponent', 'user', { user: user });
+    },
+
+    _handleAction: function(e) {
+        var action = e.currentTarget.getAttribute('data-action');
+        var selected = this.state.selected;
+        var selectedVmUuids = _.pluck(selected, 'uuid');
+
+        var self = this;
+        console.log('[VmsList] action', action);
+
+        switch (action) {
+            case 'reboot':
+                window.confirm("Are you sure you want to reboot "+selectedVmUuids.length + ' Virtual Machines?');
+                api.post('/api/vm-reboot').send(selectedVmUuids).end(function(res) {
+                    self.setState({jobs: {
+                        vms: selected,
+                        action: 'Reboot Virtual Machines',
+                        jobs: res.body
+                    }});
+                });
+                break;
+
+            case 'start':
+                window.confirm("Are you sure you want to stop " + selectedVmUuids.length + ' Virtual Machines?');
+                api.post('/api/vm-start').send(selectedVmUuids).end(function(res) {
+                    self.setState({jobs: {
+                        vms: selected,
+                        action: 'Start Virtual Machines',
+                        jobs: res.body
+                    }});
+                });
+                break;
+
+            case 'stop':
+                window.confirm("Are you sure you want to stop " + selectedVmUuids.length + ' Virtual Machines?');
+                api.post('/api/vm-stop').send(selectedVmUuids).end(function(res) {
+                    self.setState({jobs: {
+                        vms: selected,
+                        action: 'Stop Virtual Machines',
+                        jobs: res.body
+                    }});
+                });
+                break;
+        }
+    },
+
+    _handleSelectVm: function(e) {
+        var selected = this.state.selected;
+        var uuid = e.target.getAttribute('data-vm-uuid');
+        var vm = this.props.collection.get(uuid);
+        if (e.target.checked) {
+            selected.push(vm.toJSON());
+        } else {
+             var i = _.findWhere(selected, {uuid: vm.get('uuid')});
+             selected.splice(i, 1);
+        }
+        this.setState({selected: selected});
     }
 });
 
