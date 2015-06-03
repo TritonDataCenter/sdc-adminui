23676805b1ceb483e08807850326ec12b3cd2cb4
YZaicev <zaicev@silvertreesystems.com>
2015-06-03 19:39:05 +0300

    ADMINUI-2076 adminui nic tags need MTU support

--- www/js/views/nictag.js
+++ www/js/views/nictag.js
@@ -18,6 +18,9 @@ var Servers = require('../models/servers');
 
 var ServersListComponent = React.createFactory(require('../components/servers-list'));
 var NotesComponent = React.createFactory(require('../components/notes'));
+var EditableField = React.createFactory(require('../components/editable-field'));
+
+var api = require('../request');
 
 module.exports = Backbone.Marionette.Layout.extend({
     template: require('../tpl/nictag.hbs'),
@@ -26,41 +29,49 @@ module.exports = Backbone.Marionette.Layout.extend({
     },
     sidebar: 'networking',
     url: function() {
-        return '/nictags/' + this.model.get('name')     ;
+        return '/nictags/' + this.model.get('name');
     },
     regions: {
         "networksRegion": '.networks-region'
     },
-    initialize: function() {
+    initialize: function () {
         this.networks = new Networks();
         this.networks.params = {nic_tag: this.model.get('name')};
-        this.networksView = new NetworksListView({ collection: this.networks });
+        this.networksView = new NetworksListView({collection: this.networks});
 
         this.listenTo(this.networksView, 'itemview:select', this.showNetwork, this);
 
         this.servers = new Servers();
         this.servers.params = {nictag: this.model.get('name')};
-        this.serversView = new ServersListComponent({collection: this.servers });
+        this.serversView = new ServersListComponent({collection: this.servers});
     },
-    showNetwork: function(network) {
+    showNetwork: function (network) {
         var net = network.model;
         adminui.vent.trigger('showview', 'network', {model: net});
     },
-
-    onShow: function() {
-        this.networks.fetch().done(function() {
+    onShow: function () {
+        this.networks.fetch().done(function () {
             this.networksRegion.show(this.networksView);
         }.bind(this));
 
         React.render(ServersListComponent({collection: this.servers}), this.$('.servers-region').get(0));
     },
-
-    onRender: function() {
+    mtuUpdate: function (value, params, callback) {
+        params.mtu = /^[0-9]+$/.test(value) ? parseInt(value, 10) : value;
+        api.put('/api/nic_tags').send(params).end(callback);
+    },
+    onRender: function () {
         React.render(
             NotesComponent({ item: this.model.get('uuid')}),
             this.$('.notes-component-container').get(0)
         );
+        React.render(
+            EditableField({
+                value: this.model.get('mtu'), 
+                title: 'MTU', 
+                onSave: this.mtuUpdate, 
+                params: {name: this.model.get('name')}
+            }), this.$('.mtu-field').get(0)
+        );
     }
-
-
 });
