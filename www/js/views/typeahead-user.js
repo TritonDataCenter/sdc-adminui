e2a676c1eaf0c9c82e7947d8d042c1636d482254
YZaicev <zaicev@silvertreesystems.com>
2015-06-23 15:03:38 +0300

    ADMINUI-2099 VM owner uuid search options ghosting

--- www/js/views/typeahead-user.js
+++ www/js/views/typeahead-user.js
@@ -31,47 +31,53 @@ var UserTypeaheadView = Backbone.Marionette.View.extend({
 
     template: UserTypeaheadTpl,
 
-    initialize: function(options) {
+    initialize: function (options) {
         options = options || {};
         this.elemValue = this.$el.val();
         this.selectedUser = null;
+        this.preSelectedUser = options.preSelectedUser;
     },
 
-    onTypeaheadSelect: function(e, datum) {
+    onTypeaheadSelect: function (e, datum) {
         console.debug('typeahead selected', e, datum);
         this.$el.tooltip('destroy');
         this.selectedUser = datum.model;
         this.trigger('selected', datum.model);
         if (this.options.showPreview) {
-            this.$el.after(new UserPreview({model: datum.model, previewType: this.options.previewType || 'short'}).render().el);
+            this.$el.after(new UserPreview({
+                model: datum.model,
+                previewType: this.options.previewType || 'short'
+            }).render().el);
         }
     },
 
-    clearField: function(force) {
-        process.nextTick(function() {
+    clearField: function (force) {
+        process.nextTick(function () {
             this.$el.val(force ? '' : this.elemValue);
         }.bind(this));
     },
 
-    onOpened: function() {
-        this.selectedUser = null;
-        this.trigger('selected', null);
+    onOpened: function () {
+        var user = this.preSelectedUser || this.$el.val();
+        this.selectedUser = user;
+        this.trigger('selected', user);
     },
 
-    onTypeaheadClosed: function(e, suggestion, dataset) {
+    onTypeaheadClosed: function (e, suggestion, dataset) {
         console.debug('typeahead closed');
         var $field = this.$el;
-
-        if (this.selectedUser && this.selectedUser.attributes && $field.val() === this.selectedUser.get('uuid')) {
-            return;
-        }
-
-        if ($field.val().length !== 36 && this.options.showPreview) {
-            this.$el.parent().find('.user-preview').remove();
+        var value = $field.val();
+        var selectedUser = this.selectedUser;
+        if ((typeof selectedUser === 'object' && selectedUser.get && selectedUser.get('uuid') || selectedUser) !== value) {
+            if (value.length !== 36 && this.options.showPreview) {
+                this.$el.parent().find('.user-preview').remove();
+            } else {
+                this.trigger('selected', value);
+            }
         }
     },
 
-    initializeEngine: function() {
+    initializeEngine: function () {
         var self = this;
         var url = this.options.accountsOnly ? '/api/users?accountsonly=true&q=%QUERY' : '/api/users?q=%QUERY';
         this.engine = new Bloodhound({
@@ -80,65 +86,70 @@ var UserTypeaheadView = Backbone.Marionette.View.extend({
             remote: {
                 url: url,
                 ajax: {
-                    beforeSend: function(xhr) {
+                    beforeSend: function (xhr) {
                         if (!this.headers['x-adminui-token'] && app.user && app.user.getToken()) {
                             xhr.setRequestHeader('x-adminui-token', app.user.getToken());
                         }
                         self.showLoading();
                     }
                 },
-                filter: function(users) {
+                filter: function (users) {
                     self.hideLoading();
-                    var datums = users.map(function(u) {
+                    var datums = users.map(function (user) {
                         return {
-                            model: new User(u),
-                            'uuid': u.uuid,
-                            'name': u.cn,
-                            'login': u.login,
-                            'alias': u.alias,
-                            'email': u.email
+                            model: new User(user),
+                            'uuid': user.uuid,
+                            'name': user.cn,
+                            'login': user.login,
+                            'alias': user.alias,
+                            'email': user.email
                         };
                     });
                     return datums;
                 }
             },
             queryTokenizer: Bloodhound.tokenizers.whitespace,
-            datumTokenizer: function(u) {
-                return [u.login, u.uuid, u.email];
+            datumTokenizer: function (user) {
+                return [user.login, user.uuid, user.email];
             }
         });
         this.engine.initialize();
         this.$el.typeahead({
-                name: 'users',
-                minLength: 1,
-                highlight: true
-            },
-            {
-                displayKey: 'uuid',
-                name: 'users',
-                source: this.engine.ttAdapter(),
-                templates: {
-                    suggestion: UserTypeaheadTpl
-                }
-            });
+            name: 'users',
+            minLength: 1,
+            highlight: true
+        }, {
+            displayKey: 'uuid',
+            name: 'users',
+            source: this.engine.ttAdapter(),
+            templates: {
+                suggestion: UserTypeaheadTpl
+            }
+        });
+        if (this.preSelectedUser) {
+            this.$el.typeahead('val', this.preSelectedUser);
+            this.preSelectedUser = '';
+            this.$el.typeahead('close');
+        }
     },
 
-    showLoading: function() {
-        if (this.$el.parent().parent().find('.tt-loading').length) {
-            this.$el.parent().parent().find(".tt-loading").show();
+    showLoading: function () {
+        var typeaheadLoading = this.$el.parent().parent().find('.tt-loading');
+        if (typeaheadLoading.length) {
+            typeaheadLoading.show();
         } else {
             this.$el.parent().after("<div class='tt-loading'>Loading</div>");
         }
     },
 
-    hideLoading: function() {
+    hideLoading: function () {
         this.$el.parent().parent().find(".tt-loading").hide();
     },
 
-    render: function() {
+    render: function () {
         this.initializeEngine();
     },
-    remove: function() {
+    remove: function () {
         this.$el.typeahead('destroy');
         return Backbone.Marionette.View.prototype.remove.call(this);
     }
