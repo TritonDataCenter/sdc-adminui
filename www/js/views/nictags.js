5788da822067c7eac11b2f9010a3b063cdd3b53f
Andrey Yatskov <andrey.yatskov@silvertreesystems.com>
2015-07-03 13:59:53 +0300

    ADMINUI-2122 Should be able to delete unused NIC Tag

--- www/js/views/nictags.js
+++ www/js/views/nictags.js
@@ -7,7 +7,7 @@
  */
 
 /*
- * Copyright (c) 2014, Joyent, Inc.
+ * Copyright (c) 2015, Joyent, Inc.
  */
 
 var adminui = require('adminui');
@@ -22,21 +22,24 @@ var NicTagForm = require('./nictag-form');
 var app = require('../adminui');
 
 var NicTagsPage = React.createClass({
-    getInitialState: function() {
+    getInitialState: function () {
         return {
             form: (this.props.form || false),
             formData: {}
         };
     },
-    _showForm: function() {
+    _showForm: function () {
         this.setState({form: true});
     },
-    _hideForm: function() {
+    _hideForm: function () {
         this.setState({form: false});
     },
-    handleSave: function(params) {
+    setError: function (err) {
+        this.setState({error: err});
+    },
+    handleSave: function (params) {
         params.mtu = /^[0-9]+$/.test(params.mtu) ? parseInt(params.mtu, 10) : params.mtu;
-        api.post('/api/nic_tags').send(params).end(function(res) {
+        api.post('/api/nic_tags').send(params).end(function (res) {
             if (res.error) {
                 this.setState({error: res.body});
                 return;
@@ -50,9 +53,9 @@ var NicTagsPage = React.createClass({
             }
         }.bind(this));
     },
-    render: function() {
+    render: function () {
         return <div className="nic-tags">
-            <h3>Nic Tags
+            <h3>NIC Tags
                 <div className="actions">
                     {app.user.role('operators') &&  
                     <button className="btn btn-info" onClick={this._showForm}>
@@ -60,38 +63,67 @@ var NicTagsPage = React.createClass({
                     </button>}
                 </div>
             </h3>
-            { this.state.error && <ErrorAlert error={this.state.error} /> }
-            { this.state.form && <NicTagForm handleClose={this._hideForm} handleSave={this.handleSave}/> }
-            { !this.state.form && <NicTagsList /> }
+            {this.state.error && <ErrorAlert error={this.state.error} />}
+            {this.state.form && <NicTagForm handleClose={this._hideForm} handleSave={this.handleSave} />}
+            {!this.state.form && <NicTagsList handleError={this.setError} />}
         </div>;
     }
 });
 
 var NicTagsList = React.createClass({
-    getInitialState: function() {
+    getInitialState: function () {
         return {
             data: []
         };
     },
-    componentWillMount: function() {
-        var nicTags = new NicTags();
+    componentWillMount: function () {
         var self = this;
-        var promise = nicTags.fetch();
-        $.when(promise).then(function() {
-            self.setState({data: nicTags});
+        this.nicTags = new NicTags();
+        var promise = this.nicTags.fetch();
+        $.when(promise).then(function () {
+            self.setState({data: self.nicTags});
         });
     },
-    onClick: function(nictag) {
-        adminui.vent.trigger('showview', 'nictag', { model: nictag });
+    onClick: function (nictag) {
+        adminui.vent.trigger('showview', 'nictag', {model: nictag});
         return false;
     },
-    render: function() {
-        var nodes = this.state.data.map(function(nictag) {
+    onClickDeleteNicTag: function (nictag) {
+        var self = this;
+        var confirm = window.confirm(
+            _.str.sprintf('Are you sure you want to delete the NIC tag "%s" ?', nictag.name)
+        );
+        if (confirm) {
+            api.del('/api/nic_tags/' + nictag.name).end(function (res) {
+                if (res.error) {
+                    self.props.handleError(res.body);
+                    return;
+                }
+                if (res.ok) {
+                    self.nicTags.fetch().done(function () {
+                        self.props.handleError(null);
+                        self.setState({data: self.nicTags});
+                    });
+
+                    adminui.vent.trigger('notification', {
+                        level: 'success',
+                        message: _.str.sprintf('NIC tag <strong>%s</strong> deleted successfully', nictag.name)
+                    });
+                }
+            }.bind(this));
+        }
+    },
+    render: function () {
+        var nodes = this.state.data.map(function (nictag) {
             var nc = nictag.toJSON();
             var url = "/nictags/" + nc.name;
             return (
                 <li key={nc.name}>
-                    <a onClick={this.onClick.bind(this, nictag)} data-uuid={nc.name} href={url}>{nc.name}</a><br/><span>mtu: {nc.mtu}</span>  
+                    <a onClick={this.onClick.bind(this, nictag)} data-uuid={nc.name} href={url}>{nc.name}</a><br/><span className="mtu">mtu: {nc.mtu}</span>
+                    {app.user.role('operators') &&
+                    <div className="actions">
+                        <a onClick={this.onClickDeleteNicTag.bind(this, nc)} className="delete-nictag"><i className="fa fa-trash-o"></i> Delete</a>
+                    </div>}
                 </li>
                 );
         }, this);
