428926d2ab486f5f7180bec05387e03ed344d441
dboldyrev <Arc3111@gmail.com>
2016-07-11 13:43:46 +0300

    ADMINUI-2229 Want Filter on Services Page: Manta, SDC, or Both

--- www/js/views/services.js
+++ www/js/views/services.js
@@ -9,32 +9,43 @@
  */
 
 var $ = require('jquery');
-
+var _ = require('underscore');
 var Backbone = require('backbone');
 var adminui = require('../adminui');
 var Services = require('../models/services');
+var Model = require('../models/model');
+var Collection = require('../models/collection');
 var CollectionView = require('./collection');
+var DEFAULT_TYPE = 'all';
 
-var Instance = require('../models/model').extend({
+var Instance = Model.extend({
     idAttribute: 'uuid',
     urlRoot: '/api/instances'
 });
 
-var Instances = require('../models/collection').extend({
+var Instances = Collection.extend({
     model: Instance,
     url: '/api/instances'
 });
 
-var Application = require('../models/model').extend({
+var Application = Model.extend({
     idAttribute: 'uuid',
     urlRoot: '/api/applications'
 });
 
-var Applications = require('../models/collection').extend({
+var Applications = Collection.extend({
     model: Application,
-    url: '/api/applications'
+    url: '/api/applications',
+    filterByType: function (type) {
+        var applications = null;
+        if (type !== DEFAULT_TYPE) {
+            applications = this.filter(function (application) {
+                return application.attributes.name.toLowerCase() === type;
+            });
+        }
+        return applications && new Applications(applications) || this;
+    }
 });
-
 
 var InstanceView = Backbone.Marionette.ItemView.extend({
     template: require('../tpl/services-instance.hbs'),
@@ -44,19 +55,19 @@ var InstanceView = Backbone.Marionette.ItemView.extend({
     attributes: {
         'class': 'instance'
     },
-    navigateToVm: function() {
+    navigateToVm: function () {
         adminui.router.showVm(this.model.get('uuid'));
     },
-    serializeData: function() {
+    serializeData: function () {
         var data = this.model.toJSON();
         data.vm = data.type === 'vm';
         data.agent = data.type === 'agent';
         return data;
     },
-    onRender: function() {
+    onRender: function () {
         var self = this;
         if (this.model.get('type') === 'vm') {
-            $.get('/api/vms/'+this.model.get('uuid')).done(function(res) {
+            $.get('/api/vms/' + this.model.get('uuid')).done(function (res) {
                 self.$('.state').addClass(res.state).html(res.state);
                 self.$('.ram').addClass(res.state).html(res.ram + ' MB');
             });
@@ -70,22 +81,28 @@ var InstancesView = CollectionView.extend({
 
 var ServicesListItemView = Backbone.Marionette.ItemView.extend({
     template: require('../tpl/services-item.hbs'),
-    attributes: { 'class': 'service' },
-    initialize: function() {
+    attributes: {class: 'service'},
+    initialize: function () {
         this.instances = new Instances();
     },
-    onRender: function() {
-        this.instancesView = new InstancesView({
+    onRender: function () {
+        new InstancesView({
             el: this.$('.instances'),
             collection: this.instances
         });
-        this.instances.params = { service_uuid: this.model.get('uuid') };
+        this.instances.params = {service_uuid: this.model.get('uuid')};
         this.instances.fetch({reset: true});
     }
 });
 
 var ServicesListView = CollectionView.extend({
     itemView: ServicesListItemView
+});
+
+var NoApplicationsView = Backbone.Marionette.ItemView.extend({
+    template: function () {
+        return _.template('<div class="zero-state">No services available.</div>');
+    }
 });
 
 var ApplicationsListView = CollectionView.extend({
@@ -99,7 +116,7 @@ var ApplicationsListView = CollectionView.extend({
         },
         onRender: function () {
             var self = this;
-            this.servicesView = new ServicesListView({
+            new ServicesListView({
                 el: this.$('.services'),
                 collection: this.services
             });
@@ -108,23 +125,40 @@ var ApplicationsListView = CollectionView.extend({
                 self.$('.number-of-services').html(res.length);
             }});
         }
-    })
+    }),
+    emptyView: NoApplicationsView
 });
 
 var View = Backbone.Marionette.ItemView.extend({
     id: 'page-services',
     template: require('../tpl/services.hbs'),
+    events: {
+        'click a[data-service-type]': 'onChangeView'
+    },
     name: 'services',
-    url: function() {
+    url: function () {
         return '/services';
     },
-
-    initialize: function() {
+    initialize: function () {
         this.applications = new Applications();
+        this.type = DEFAULT_TYPE;
     },
-
-    onRender: function() {
+    makeActive: function (type) {
+        this.$('[data-service-type=' + type + ']').parent().addClass('active').siblings().removeClass('active');
+    },
+    onChangeView: function (e) {
+        e.preventDefault();
+        var newType = e.target.getAttribute('data-service-type');
+        if (this.type !== newType) {
+            this.type = newType;
+            this.makeActive(newType);
+            this.applicationsView.collection = this.applications.filterByType(newType);
+            this.applicationsView.render();
+        }
+    },
+    onRender: function () {
         adminui.vent.trigger('settitle', 'services');
+        this.makeActive(DEFAULT_TYPE);
 
         this.applicationsView = new ApplicationsListView({
             el: this.$('.applications'),
