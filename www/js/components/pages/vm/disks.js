96a6835a601205897d9b5b361afd4aa01d53fd0d
Pedro Palazón Candel <kusorbox@gmail.com>
2019-12-26 08:53:51 +0100

    TRITON-1790 Add/resize/remove disk actions to be added to VM#view page

--- www/js/components/pages/vm/disks.js
+++ www/js/components/pages/vm/disks.js
@@ -10,32 +10,90 @@
  */
 
 var Backbone = require('backbone');
-var Disks = Backbone.Collection.extend({});
 
+/**
+ * Disks Table/List
+ */
+var Disks = Backbone.Collection.extend({});
+var JobProgressView = require('../../../views/job-progress');
+var DisksFormView = require('./disks-form');
 var DiskRowTemplate = require('./disks-row.hbs');
+
+var adminui = require('../../../adminui');
 var DiskRow = Backbone.Marionette.ItemView.extend({
     tagName: 'tr',
     template: DiskRowTemplate,
     events: {
+        'click .edit': 'resizeDisk',
+        'click .delete': 'deleteDisk'
     },
-    initialize: function (options)  {
-        // Override list initialization
+
+    deleteDisk: function () {
+        if (window.confirm('Are you sure you want to delete this disk?')) {
+            this.vm.removeDisk(this.model.get('pci_slot'),
+                this._onJob.bind(this));
+        }
+    },
+    _onJob: function (err, job) {
+        if (err) {
+            console.log('[job error]: ', err);
+            var msg = (err.responseText) ?
+                    JSON.parse(err.responseText).message:
+                    'Error creating job';
+            adminui.vent.trigger('notification', {
+                level:'error',
+                message: msg
+            });
+            return;
+        }
+        var self = this;
+        var jobView = new JobProgressView({model: job});
+        this.listenTo(jobView, 'execution', function (exec) {
+            if (exec === 'succeeded') {
+                self.vm.fetch();
+            }
+        });
+        jobView.show();
+    },
+
+    resizeDisk: function () {
+        var self = this;
+        var view = new DisksFormView({
+            vm: self.vm,
+            model: self.model
+        });
+        view.render();
     }
+
 });
 
 var DisksView = Backbone.Marionette.CompositeView.extend({
-    template: require('./disks.hbs'),
     itemView: DiskRow,
     itemViewContainer: 'tbody',
+    template: require('./disks.hbs'),
     attributes: {
         id: 'vm-disks'
     },
     events: {
+        'click button': 'onClickAddDisk'
     },
 
     initialize: function (options)  {
         this.vm = options.vm;
         this.collection = new Disks(this.vm.get('disks'));
+        this.listenTo(this.collection, "add", this.render, this);
+        this.listenTo(this.collection, "remove", this.render, this);
+        this.listenTo(this.collection, "sync", this.render, this);
+        this.listenTo(this.vm, 'change:disks', this.resetDisks, this);
+    },
+
+    resetDisks: function(vm, n) {
+        this.collection.reset(vm.get('disks'));
+        this.render();
+    },
+
+    onBeforeItemAdded: function(view) {
+        view.vm = this.vm;
     },
 
     templateHelpers: function() {
@@ -43,10 +101,45 @@ var DisksView = Backbone.Marionette.CompositeView.extend({
         return {
             disks: function() {
                 return self.collection;
+            },
+
+            hasDisks: function(data) {
+                return self.collection.length > 0;
             }
         };
-    }
+    },
 
+    onClickAddDisk: function () {
+        var self = this;
+        var view = new DisksFormView({
+            vm: self.vm
+        });
+        view.render();
+    },
+
+    clickedCreateDisk: function() {
+        var vm = this.vm;
+        var self = this;
+        this.vm.createDisk(function(job, err) {
+            if (err) {
+                console.log('[Disks] error: ', err);
+                adminui.vent.trigger('notification', {
+                    level:'error',
+                    message: err.message
+                });
+                return;
+            }
+            var jobView = new JobProgressView({
+                model: job
+            });
+            self.listenTo(jobView, 'execution', function(exec) {
+                if (exec === 'succeeded') {
+                    vm.fetch();
+                }
+            });
+            jobView.show();
+        });
+    }
 });
 
 
--- www/js/components/pages/vm/fw-toggle-button.jsx
+++ www/js/components/pages/vm/fw-toggle-button.jsx
@@ -1,4 +1,14 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 
+/*
+ * Copyright 2019 Joyent, Inc.
+ */
+
+/* eslint-disable max-len */
 var React = require('react');
 
 
